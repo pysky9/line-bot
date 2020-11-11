c2b53f4f76352c8f40aa823c098cd3d4082f4a27
Derek Li <derek.j.h.li@gmail.com>
2020-11-11 20:36:13 +0800

    update msg

--- app.py
+++ app.py
@@ -38,7 +38,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = "你吃飯了嗎"
+    s = "很抱歉，我不知道你再說甚麼"
+    if msg == "hi":
+        s = "hi"
+    elif msg == "你好嗎":
+        s = "我很好，你呢"
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
